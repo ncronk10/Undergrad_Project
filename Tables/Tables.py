defbb4238728e09a2e1ccad6ecf2f07479dc86ec
ncronk10@gmail.com <ncronk10@gmail.com>
2023-08-25 18:29:56 +0000

    master table

--- Tables/Tables.py
+++ Tables/Tables.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 # DBTITLE 1,Players Table
-players = spark.table('default.players')
+players = spark.table('default.players').withColumnRenamed('_id','player_id')
 #display(players)
 
 # COMMAND ----------
@@ -8,3 +8,9 @@ players = spark.table('default.players')
 # DBTITLE 1,Salaries Table
 salaries = spark.table('default.salaries')
 #display(salaries)
+
+# COMMAND ----------
+
+# DBTITLE 1,Master Table
+master = players.join(salaries,['player_id'], how='left')
+#display(master)
