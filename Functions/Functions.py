acf7f0800084e8f69c54b0a3d37b1cee44e6b1f7
ncronk10@gmail.com <ncronk10@gmail.com>
2023-10-23 18:10:28 +0000

    Changes

--- Functions/Functions.py
+++ Functions/Functions.py
@@ -1,5 +1,6 @@
 # Databricks notebook source
 import pyspark.sql.functions as F
+from pyspark.sql.window import Window
 from pyspark.sql.functions import concat, col, lit, current_timestamp, current_date, date_add, to_date, when, substring, month, sum, count, trim, unix_timestamp, from_unixtime, to_date, countDistinct, date_sub, array_join, concat_ws, collect_list, pow, first, row_number, date_format, split
 from pyspark.sql.types import ArrayType, FloatType, StringType, IntegerType
 
@@ -9,12 +10,12 @@ from pyspark.sql.types import ArrayType, FloatType, StringType, IntegerType
 
 # COMMAND ----------
 
-# DBTITLE 1,PlayerCheck 
-def playerCheck(playerID: list = [], playerName: list = [], position: list = [], height: list = [], weight: list = [], college: list = [], draftTeam: list = [], draftRound: list = [], draftPick: list = [], draftYear: list =[]):
+# DBTITLE 1,PlayerCheck Function
+def playerCheck(playerID: list = [], playerName: list = [], position: list = [], college: list = [], draftTeam: list = [], draftRound: list = [], draftPick: list = [], draftYear: list =[]):
 
     df = (players
           .select('player_Id','name','position','height','weight','college','draft_Team','draft_Round','draft_Pick','draft_Year')
-          .dropna())
+          )
     
     try:
         if playerID:
@@ -25,12 +26,6 @@ def playerCheck(playerID: list = [], playerName: list = [], position: list = [],
 
         if position:
             df = df.where(F.col('position').isin(position))
-        
-        if height:
-            df = df.where(F.col('height').isin(height))
-
-        if weight:
-            df = df.where(F.col('weight').isin(weight))
     
         if college:
             df = df.where(F.col('college').isin(college))
@@ -55,9 +50,9 @@ def playerCheck(playerID: list = [], playerName: list = [], position: list = [],
 
 # COMMAND ----------
 
-# DBTITLE 1,topSalary
+# DBTITLE 1,topSalary Function
 #Create a function that takes in a dataframe, round, pick, top x% of players based on salary and returns them
-def topSalary(df, draftRound: list=[], draftPick: list=[]):
+def topSalary(df, draftRound: list=[], draftPick: list=[], contract_Length:int=0):
 
     playersDF = players
     salariesDF = salaries
@@ -68,7 +63,7 @@ def topSalary(df, draftRound: list=[], draftPick: list=[]):
               .withColumnRenamed('_id','player_Id')
               .select('player_Id', 'salary')
               .dropna()
-              .withColumn('yearly_Average',F.col('salary')/4)
+              .withColumn('yearly_Average',F.col('salary')/contract_Length)
               )
     
     masterDF= df.join(joinDF, ['player_Id'], how='inner')
@@ -83,19 +78,8 @@ def topSalary(df, draftRound: list=[], draftPick: list=[]):
     except: 
         
         print("The input given to the function was not valid. Please try again.")
+    
+    window = Window.partitionBy('player_Id').orderBy(F.col('salary').desc())
+    filterDF = masterDF.withColumn('temp', row_number().over(window)).filter(F.col('temp') == 1).drop('temp')
 
-    return masterDF.orderBy(F.col('salary').desc())
-
-# COMMAND ----------
-
-playerDF = playerCheck(draftRound=['1st round'])
-display(playerDF)
-
-# COMMAND ----------
-
-d2 = topSalary(playerDF)
-display(d2)
-
-# COMMAND ----------
-
-
+    return filterDF.orderBy(F.col('salary').desc()).orderBy(F.col('yearly_Average').desc())
