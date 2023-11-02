673ea2805ad4b50b890d9488ac07f056b5687466
ncronk10@gmail.com <ncronk10@gmail.com>
2023-11-02 16:24:48 +0000

    Added TopSalary by Team function

--- Functions/Functions.py
+++ Functions/Functions.py
@@ -1,28 +1,22 @@
 # Databricks notebook source
-import pyspark.sql.functions as F
-from pyspark.sql.window import Window
-from pyspark.sql.functions import concat, col, lit, current_timestamp, current_date, date_add, to_date, when, substring, month, sum, count, trim, unix_timestamp, from_unixtime, to_date, countDistinct, date_sub, array_join, concat_ws, collect_list, pow, first, row_number, date_format, split
-from pyspark.sql.types import ArrayType, FloatType, StringType, IntegerType
-
-# COMMAND ----------
-
 # MAGIC %run /Repos/ncronk10@gmail.com/Undergrad_Project/Tables/Tables
 
 # COMMAND ----------
 
 # DBTITLE 1,PlayerCheck Function
-def playerCheck(playerID: list = [], playerName: list = [], position: list = [], college: list = [], draftTeam: list = [], draftRound: list = [], draftPick: list = [], draftYear: list =[]):
-
+def playerCheck(playerID: list = [], position: list = [], college: list = [], draftTeam: list = [], draftRound: list = [], draftPick: list = [], draftYear: list =[]):
+    
     df = (players
-          .select('player_Id','name','position','height','weight','college','draft_Team','draft_Round','draft_Pick','draft_Year')
-          )
+          .select('player_Id','player_Name','position','height','weight','college','draft_Team','draft_Round','draft_Pick','draft_Year')
+          .withColumn('draft_Round', F.regexp_extract('draft_Round', '\d{1,3}(?!\d)', 0))
+          .withColumn('draft_Pick', F.regexp_extract('draft_Pick', '\d{1,3}(?!\d)', 0))
+          .withColumn('draft_Round', F.col('draft_Round').cast(IntegerType()))
+          .withColumn('draft_Pick', F.col('draft_Pick').cast(IntegerType()))
+    )
     
     try:
         if playerID:
             df = df.where(F.col('player_Id').isin(playerID))
-
-        if playerName:
-            df = df.where(F.col('name').isin(playerName))
 
         if position:
             df = df.where(F.col('position').isin(position))
@@ -41,6 +35,15 @@ def playerCheck(playerID: list = [], playerName: list = [], position: list = [],
 
         if draftYear:
             df = df.where(F.col('draft_Year').isin(draftYear))
+
+        split_text = split(F.col("position"), "\\s+")
+        df = df.withColumn("extracted_words", concat_ws(" ", split_text[0], split_text[1]))
+        df = (df
+                .withColumn("position", F.regexp_replace(F.col("extracted_words"), "Forward/Center", "Center"))
+                .withColumn("position", F.regexp_replace(F.col("extracted_words"), "Center and", "Center"))
+                .drop('extracted_words')
+        )
+        
         
     except:
 
@@ -51,8 +54,7 @@ def playerCheck(playerID: list = [], playerName: list = [], position: list = [],
 # COMMAND ----------
 
 # DBTITLE 1,topSalary Function
-#Create a function that takes in a dataframe, round, pick, top x% of players based on salary and returns them
-def topSalary(df, draftRound: list=[], draftPick: list=[], contract_Length:int=0):
+def topSalary(df, draftRound: list=[], draftPick: list=[], contract_Length:int=4):
 
     playersDF = players
     salariesDF = salaries
@@ -67,8 +69,7 @@ def topSalary(df, draftRound: list=[], draftPick: list=[], contract_Length:int=0
               )
     
     masterDF= df.join(joinDF, ['player_Id'], how='inner')
-
-    #df = playerCheck(df)
+    
     try: 
         if draftRound:
             masterDF = masterDF.where(F.col('draft_Round').isin(draftRound))
@@ -83,3 +84,33 @@ def topSalary(df, draftRound: list=[], draftPick: list=[], contract_Length:int=0
     filterDF = masterDF.withColumn('temp', row_number().over(window)).filter(F.col('temp') == 1).drop('temp')
 
     return filterDF.orderBy(F.col('salary').desc()).orderBy(F.col('yearly_Average').desc())
+
+# COMMAND ----------
+
+# DBTITLE 1,TeamSalaryPerYear
+def TeamSalaryPerYear(df): 
+
+    playersDF = players
+    salariesDF = salaries
+
+    fin = playerCheck()
+
+    joinDF = (playersDF.join(salariesDF, ['player_id'], how='left')
+              .withColumnRenamed('_id','player_Id')
+              .select('player_Id', 'salary')
+              .dropna()
+              )
+    
+    masterDF= df.join(joinDF, ['player_Id'], how='inner') 
+
+    window = Window.partitionBy('player_Id').orderBy(F.col('salary').desc())
+    filterDF = masterDF.withColumn('temp', row_number().over(window)).filter(F.col('temp') == 1).drop('temp')
+
+    result = filterDF.groupBy('draft_Team', 'draft_Year').agg(F.sum('salary').alias('total_Salary'))
+
+    return result.orderBy(F.col('draft_Year')).orderBy(F.col('total_Salary'))
+
+
+# COMMAND ----------
+
+
--- Tables/Tables.py
+++ Tables/Tables.py
@@ -1,7 +1,15 @@
 # Databricks notebook source
+import pyspark.sql.functions as F
+from pyspark.sql.window import Window
+from pyspark.sql.functions import concat, col, lit, current_timestamp, current_date, date_add, to_date, when, substring, month, sum, count, trim, unix_timestamp, from_unixtime, to_date, countDistinct, date_sub, array_join, concat_ws, collect_list, pow, first, row_number, date_format, split
+from pyspark.sql.types import IntegerType
+
+# COMMAND ----------
+
 # DBTITLE 1,Players Table
 players = (spark.table('default.players')
            .withColumnRenamed('_id','player_Id')
+           .withColumnRenamed('name','player_Name')
            .withColumnRenamed('birthDate', 'birth_Date')
            .withColumnRenamed('birthPlace', 'birth_Place')
            .withColumnRenamed('career_AST', 'career_Ast')
@@ -9,6 +17,7 @@ players = (spark.table('default.players')
            .withColumnRenamed('draft_round', 'draft_Round')
            .withColumnRenamed('draft_team', 'draft_Team')
            .withColumnRenamed('draft_year', 'draft_Year')
+           .withColumn('draft_Year', F.col('draft_Year').cast(IntegerType()))
            .drop('career_PER','career_TRB', 'career_WS', 'career_eFG%', 'shoots')
            .dropna())
 
@@ -20,3 +29,7 @@ salaries = (spark.table('default.salaries')
             .withColumnRenamed('season_end', 'season_End')
             .withColumnRenamed('season_start', 'season_Start')
             .dropna())
+
+# COMMAND ----------
+
+
