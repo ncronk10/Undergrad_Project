8e604643f12ed6c66321819c4a7f4d827b5a6d2d
ncronk10 <107149561+ncronk10@users.noreply.github.com>
2023-12-01 13:25:56 -0600

    Update NBA_Tables.py

--- NBA_Tables.py
+++ NBA_Tables.py
@@ -1,5 +1,1 @@
-# Databricks notebook source
-import pyspark.sql.functions as F
-from pyspark.sql.window import Window
-from pyspark.sql.functions import concat, col, lit, current_timestamp, current_date, date_add, to_date, when, substring, month, sum, count, trim, unix_timestamp, from_unixtime, to_date, countDistinct, date_sub, array_join, concat_ws, collect_list, pow, first, row_number, date_format, split
-from pyspark.sql.types import IntegerType
+
