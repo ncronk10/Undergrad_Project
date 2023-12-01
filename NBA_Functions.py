4a904994acb5cc96a911fedb467cdfe309c42950
ncronk10 <107149561+ncronk10@users.noreply.github.com>
2023-12-01 12:47:36 -0600

    Update NBA_Functions.py

--- NBA_Functions.py
+++ NBA_Functions.py
@@ -6,6 +6,11 @@
 
 import pyspark.sql.functions as F
 
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+sc = SparkContext('local')
+spark = SparkSession(sc)
+
 from pyspark.sql.window import Window
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import concat, col, lit, current_timestamp, current_date, date_add, to_date, when, substring, month, sum, count, trim, unix_timestamp, from_unixtime, to_date, countDistinct, date_sub, array_join, concat_ws, collect_list, pow, first, row_number, date_format, split
